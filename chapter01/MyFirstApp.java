e31c073888baf7234e7965dcb3b5a8bcb40b7dc3
Steven Lozano <stevenlozano17@gmail.com>
2024-07-01 13:42:46 -0500

    chapter 01, page 9

--- chapter01/MyFirstApp.java
+++ chapter01/MyFirstApp.java
@@ -1,6 +1,6 @@
 public class MyFirstApp {
-	public static void main (String[] args){
-		System.out.println("I Rule!");
-		System.out.println("The World");
-	}
+  public static void main (String[] args){
+    System.out.println("I Rule!");
+    System.out.println("The World");
+  }
 }
